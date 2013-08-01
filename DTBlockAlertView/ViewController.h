96cbd4773a0da6f850ceda984f871b6af0992a69
darktt <pippipig@gmail.com>
2013-08-01 10:52:59 +0800

    Initial Commit

---
+++ DTBlockAlertView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DTBlockAlertView
+//
+//  Created by Darktt on 13/8/1.
+//  Copyright (c) 2013 Darktt Personal Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ DTBlockAlertView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  DTBlockAlertView
+//
+//  Created by Darktt on 13/8/1.
+//  Copyright (c) 2013 Darktt Personal Company. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
